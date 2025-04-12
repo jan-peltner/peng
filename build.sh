fa430ca42eee07a368de014449b983789a59bb91
jan <jan.peltner@ggmgastro.com>
2025-04-12 12:41:42 +0200

    feat: support both macOS and linux with build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+
+CFLAGS="-Wall -Wextra"
+SRC="src/main.c src/peng/peng.c"
+OUT="-o bin/peng"
+LIBS="-lraylib -lm"
 
 set -ex
 
 mkdir -p bin
 
-gcc -Wall -Wextra -o bin/peng src/main.c src/peng/peng.c -lraylib -lm
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	#macOS build
+	echo "Building for macOS..."
+	INCLUDE_PATH="-I/opt/homebrew/include"
+	LIB_PATH="-L/opt/homebrew/lib"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	INCLUDE_PATH="/usr/local/include"
+	LIB_PATH="/usr/local/lib"
+else
+	echo "Unsupported OS. Exiting compilation."
+	exit 1
+fi
+
+
+gcc $SRC $CFLAGS $INCLUDE_PATH $LIB_PATH $LIBS $OUT
